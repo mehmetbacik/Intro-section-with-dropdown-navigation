afa2297f3b13ca353a1b4e98b7a5793f9c9c3b8a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2022-12-12 19:02:24 +0300

    Update
    Updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var $closebutton = document.querySelector(".btn-close");
+$closebutton.addEventListener('click', function () {
+  document.getElementById("navbarSupportedContent").classList.add("d-none"); 
+});
+
+var $openbutton = document.querySelector(".navbar-toggler");
+    $openbutton.addEventListener('click', function () {	    
+    document.getElementById("navbarSupportedContent").classList.remove("d-none");
+});
+
 (function() {
     document.querySelector('.dropdownone').addEventListener('click', function() {
       this.parentNode.parentNode.classList.toggle('open')
